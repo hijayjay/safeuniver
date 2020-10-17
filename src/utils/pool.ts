dad6a18858b95522e6302bc232e5ad5dcce001ec
lychees <lychees67@gmail.com>
2020-10-16 23:12:56 -0700

    change abi

--- src/utils/pool.ts
+++ src/utils/pool.ts
@@ -3,7 +3,7 @@ import Web3 from 'web3'
 import { provider } from 'web3-core'
 // import { Contract } from 'web3-eth-contract'
 import { AbiItem } from 'web3-utils'
-import PoolABI from '../constants/abi/Pool.json'
+import PoolABI from '../constants/abi/Pool2.json'
 
 export const getContract = (provider: provider, address: string) => {
   const web3 = new Web3(provider)
