f54cc55e2e5b466c143722f64fa660efea9398c7
JayJay <81240900+hijayjay@users.noreply.github.com>
2021-04-26 17:18:43 +0700

    Update pools.ts

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -4,16 +4,16 @@ export const Pools = [
             56: '0x3a4Ef85EB740f18566D2Eaf36232C563c686C772',
         },
         stakingTokenAddresses: {
-            56'0x3a4Ef85EB740f18566D2Eaf36232C563c686C772',
+            56: '0x3a4Ef85EB740f18566D2Eaf36232C563c686C772',
         },
         acceleratorAddresses: {
-            56'0x3a4Ef85EB740f18566D2Eaf36232C563c686C772',
+            56:'0x3a4Ef85EB740f18566D2Eaf36232C563c686C772',
         },
         name: 'Universe',
         symbol: 'Universe',
         tokenSymbol: 'Universe',
-        isWBNB: false,
-        icon: 'BUSD
+        isWBNB: 'false',
+        icon: 'BUSD'
         nftSymbol: 'TEST',
         magnification: 1,
     }
